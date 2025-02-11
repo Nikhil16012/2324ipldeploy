3b95e771472975d5705f89ca47380c4b8a3081b9
Nikhil16012 <118714881+Nikhil16012@users.noreply.github.com>
2025-02-11 23:34:13 +0530

    Update ipl.js

--- ipl.js
+++ ipl.js
@@ -3,24 +3,96 @@ var url=require("url")
 var fs=require("fs")
 var server=http.createServer((req,res)=>{
         var urldata=req.url
-        parsedurl=url.parse(urldata,true)
+       var parsedurl=url.parse(urldata,true)
 
      fs.readFile("./ipl.json","utf-8",(err,data)=>{
         if(err){
             res.write(err)
             res.end()
         }
-        else if(parsedurl.pathname=="/ipl"&&req.method=="POST"){
-         res.write(data)
+        else if(parsedurl.pathname=="/" && req.method=="GET"){
+            if(parsedurl.query.team=="csk"){
+                 var dataa=JSON.parse(data)
+                 var filtered=dataa.ipl.filter((val)=>{
+                    return val.team=="Chennai Super Kings"
+                 })
+                 res.write(JSON.stringify(filtered));
+                 res.end()
+            }
+         else if(parsedurl.query.team=="mi"){
+                var dataa=JSON.parse(data)
+                var filtered=dataa.ipl.filter((val)=>{
+                   return val.team=="Mumbai Indians"
+                })
+                res.write(JSON.stringify(filtered));
+                res.end()
+           }
+           else if(parsedurl.query.team=="kkr"){
+            var dataa=JSON.parse(data)
+            var filtered=dataa.ipl.filter((val)=>{
+               return val.team=="Kolkata Knight Riders"
+            })
+            res.write(JSON.stringify(filtered));
             res.end()
-        }
+       }
+       else if(parsedurl.query.team=="rr"){
+        var dataa=JSON.parse(data)
+        var filtered=dataa.ipl.filter((val)=>{
+           return val.team=="Rajasthan Royals"
+        })
+        res.write(JSON.stringify(filtered));
+        res.end()
+   }
+       }
+       else if(parsedurl.pathname.startsWith("/year/")){
+        var dataa=JSON.parse(data)
+            var splitedval=parsedurl.pathname.split("/");
+            var routeval=splitedval[splitedval.length-1];
+            var returnedval=dataa.ipl.filter((val)=>{
+             return val.year==routeval;
+            })
+            res.write(JSON.stringify(returnedval))
+            res.end()
+         }
         else{
             res.write("data not found")
             res.end()
         }
-     })
-
+    })
 })
+    // fs.readFile("./ipl.json","utf-8",(err,data)=>{
+    //     if(err){
+    //         throw err
+    //     }
+    //    else if(parsedurl.pathname.startsWith("/year/")){
+    //     var dataa=JSON.parse(data)
+    //         var splitedval=parsedurl.pathname.split("/");
+    //         var routeval=splitedval[splitedval.length-1];
+    //         var returnedval=dataa.ipl.filter((val)=>{
+    //          return val.year==routeval;
+    //         })
+    //         res.write(JSON.stringify(returnedval))
+    //         res.end()
+    //      }
+    //      else{
+    //         res.write(JSON.stringify(dataa))
+    //         res.end()
+    //      }
+    //   })
+      
+    // })
+    //     if(parsedurl.pathname.startsWith("/year/")){
+    //        var splitedval=parsedurl.pathname.split("/");
+    //        var routeval=splitedval[splitedval.length-1];
+    //        var returnedval=dataa.ipl.filter((val)=>{
+    //         return val.year==routeval;
+    //        })
+    //        res.write(returnedval)
+    //        res.end()
+    //     }
+    //  })
+
+
 server.listen(3007,()=>{
     console.log("server is running");
 })
